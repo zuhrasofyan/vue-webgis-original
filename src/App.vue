d08f6725e56b0e6de331832863e2ab252d6f0108
zuhrasofyan <zuhrasofyan@gmail.com>
2018-03-14 12:30:44 +0700

    vue2-leaflet implementation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="container">
+  <div class="">
+    <Navbar></Navbar>
+    <div id="map">
+      <v-map :zoom="zoom" :center="center">
+        <v-tilelayer :url="url" :attribution="attribution"></v-tilelayer>
+        <v-marker :lat-lng="marker"></v-marker>
+      </v-map>
+    </div>
     <div class="row">
       <div class="col-sm">
         <div class="alert alert-primary" role="alert">
           This is a primary alert—check it out!
         </div>
         One of three columns
-        <b-button variant="success">Succes!</b-button>
+        <b-button variant="success" v-b-modal.modal-center>Succes!</b-button>
         <b-btn variant="success">Succes!</b-btn>
       </div>
       <div class="col-sm">
@@ -16,15 +23,37 @@
         One of three columns
       </div>
     </div>
+    <!-- Modal Component -->
+    <b-modal id="modal-center" centered title="Bootstrap-Vue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navbar
+  },
+  data () {
+    return {
+      zoom: 13,
+      center: [47.413220, -1.219482],
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      // eslint-disable-next-line
+      marker: L.latLng(47.413220, -1.219482)
+    }
+  }
 }
 </script>
 
 <style>
-
+#map {
+  height: 300px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import Vue2Leaflet from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App'
@@ -9,6 +12,9 @@ import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.component('v-map', Vue2Leaflet.Map)
+Vue.component('v-tilelayer', Vue2Leaflet.TileLayer)
+Vue.component('v-marker', Vue2Leaflet.Marker)
 
 /* eslint-disable no-new */
 new Vue({
