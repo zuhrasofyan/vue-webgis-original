e46f4b017de80d59a1ce94b6c6140254cb4869e7
zuhrasofyan <zuhrasofyan@gmail.com>
2018-03-14 12:31:25 +0700

    Resolve leaflet marker image issue against webpack

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import Vue from 'vue'
+// import L to resolve marker not rendered
+import L from 'leaflet'
 import BootstrapVue from 'bootstrap-vue'
 import Vue2Leaflet from 'vue2-leaflet'
 import 'leaflet/dist/leaflet.css'
@@ -9,6 +11,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App'
 import router from './router'
+
+// delete old _getIconUrl, then add icon marker manually to display the icon.
+// This issue happened because webpack is jacking up the embedded image URLs in the CSS.
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
